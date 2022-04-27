248e897b00aa913f3df5837dcdcd30b56263ddcb
jaryarbn <grandline501@gmail.com>
2022-04-27 14:43:01 +0800

    Add smoke test and change readme.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# SimpleChat
+### 一个简单的基于Socket的多线程聊天室
---
+++ src/test/java/com/hrbnu/se202104/SmokeTest.java
@@ -0,0 +1,10 @@
+package com.hrbnu.se202104;
+
+import org.junit.jupiter.api.Test;
+
+public class SmokeTest {
+    @Test
+    void test(){
+
+    }
+}
